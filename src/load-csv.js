3f6514203c3417e622916f31f4a6b5983fccec29
elshor <elshor@gmail.com>
2018-01-14 22:08:27 +0200

    add progress report

--- src/load-csv.js
+++ src/load-csv.js
@@ -2,24 +2,30 @@ const csv = require('csv');
 const Collection = require('..').Collection;
 const fs = require('fs');
 const axios = require('axios');
+const ProgressReporter = require('./progress-reporter');
 
 function isURL(url){
 	return url.match(/^(http|https)\:\/\//) !== null;
 }
 module.exports = function(data,path,options){
+	let reporter = ProgressReporter();
+	reporter('Loading file ' + path + ' ...');
 	let text = isURL(path)? 
 		axios.get(path,options).then((res)=>res.data) : 
 		fs.readFileSync(path);
 	return Promise.resolve(text).then((text)=>{
+		reporter('Processing the file ...');
 		return new Promise((resolver,rejector)=>{
 			let mergedOptions = {auto_parse:true,audo_parse_date:true,trim:true,columns:true};
 			Object.assign(mergedOptions,options||{});
 			csv.parse(text,mergedOptions,(err,output)=>{
 				if(err){
-					console.error('Got error while parsing file',err);
+					reporter('Got error while parsing file: ' + err.toString());
+					reporter();
 					rejector(err);
 				}else{
-					console.info('Finished loading',output.length,'rows from',path);
+					reporter('Finished loading ' + output.length + ' rows from ' + path);
+					reporter();
 					resolver(output);
 				}
 			});
