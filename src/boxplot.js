81de5c8255a1503ab1e992b18d28255a6b3257e8
elshor <elshor@gmail.com>
2018-01-14 21:47:56 +0200

    add layout argument

--- src/boxplot.js
+++ src/boxplot.js
@@ -1,16 +1,17 @@
 const plotly = require('./plotly');
 const Collection = require('..').Collection;
 
-module.exports = function(data, groupField,dataField){
+module.exports = function(data, groupField,dataField,layout){
 	return plotly.call(
+		this,
 		data,
 		Collection(data).groupBy(groupField).data().map((group)=>({
 			name:group.key,
 			y: Collection(group.data).column(dataField).data(),
 			type: 'box'
-	})), {
+	})), Object.assign({
 	xaxis: {title:groupField},
 	yaxis: {title:dataField},
 	title:'Box Plot'
-	});
+	},layout));
 };
