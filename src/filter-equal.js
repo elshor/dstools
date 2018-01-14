3c9d35fedaf2e5f48ae5713dcc0afe924d582d99
elshor <elshor@gmail.com>
2018-01-14 21:52:31 +0200

    second argument can now be either single element or an array

--- src/filter-equal.js
+++ src/filter-equal.js
@@ -1,3 +1,16 @@
+/**
+ * Filter all data elements where their field equal value or included in value array
+ * @param   {Collection}      data  input collection
+ * @param   {string}          field field name
+ * @param   {Object|Object[]} value value to select or an array of possible values
+ * @returns {Collection}      Subset of input collection where the field equals or included in value
+ */
 module.exports = function(data, field,value){
-	return data.filter((row)=>row[field]===value);
+	return data.filter((row)=>{
+		if(Array.isArray(value)){
+			 return value.includes(row[field]);
+		}else{
+			return row[field]===value;
+		}
+	});
 };
