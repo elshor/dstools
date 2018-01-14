6c88b77b04325a7b16f272e6b965a31056229136
elshor <elshor@gmail.com>
2018-01-14 22:15:49 +0200

    use field function instead of property name

--- src/group-by.js
+++ src/group-by.js
@@ -1,11 +1,34 @@
-module.exports = function(data, groupField){
+const Collection = require('..').Collection;
+const getFieldFunction = require('./utils').getFieldFunction;
+
+module.exports = function(data, groupField,groupFunctions){
 	let groups = {};
+	let fieldFunction = getFieldFunction(groupField);
+	//sort data items by group
 	for(let i in data){
-		let key = data[i][groupField];
+		let key = fieldFunction(data[i]);
 		if(!groups[key]){
 			groups[key] = {key:key,data:[]};
 		}
 		groups[key].data.push(data[i]);
 	}
+	//calculate groupFunctions
+	for(let key in groups){
+		for(let field in groupFunctions){
+			groups[key][field] = dataOf(groupFunctions[field](groups[key].data));
+		}
+	}
 	return Object.values(groups);
 };
+
+function dataOf(input){
+	//if data function is defined, use it
+	if(typeof input.data === 'function'){
+		return input.data();
+	}else{
+		return input;
+	}
+}
+
+
+
