e908bf1d48471043f6f36764ab8451039d8480db
elshor <elshor@gmail.com>
2018-02-27 13:09:49 +0200

    add thisObject argument to function

--- src/do.js
+++ src/do.js
@@ -1,10 +1,10 @@
 /**
- * Run a function receiving the collection data as its first argument. This is useful when chaining several functions and there is a need for customized operation in the midst of the chain
+ * Run a function receiving the collection data as its first argument. This is useful when chaining several functions and there is a need for customized operation in the midst of the chain.
  * @alias do
- * @param   {function} func The function to execute in the form of func(data)
+ * @param   {function} func The function to execute in the form of func(data,thisObject)
  * @returns {Object}   The input wrapped data
  */
 module.exports = function(data, func){
-	func(data);
+	func.call(this,data,this);
 	return data;
 };
