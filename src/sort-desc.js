2f36cd6e3ebf82df3f6c275f302e2aa5314fa586
elshor <elshor@gmail.com>
2018-03-08 19:43:09 +0200

    FIX sort and sortDesc did not use the field function correctly

--- src/sort-desc.js
+++ src/sort-desc.js
@@ -5,22 +5,5 @@
  * @returns {Collection}             The sorted collection
  */
 module.exports = function(data, byField){
-	//create ret array
-	let ret = Array.from(data);
-	let byFieldFunction;
-	if(typeof byField !== 'function'){
-		console.assert(typeof byField === 'string','sort argument should be comparing function or name of field');
-		byFieldFunction = function(a,b){
-			if(a[byField] < b[byField]){
-				return 1;
-			}else if(a[byField] > b[byField]){
-				return -1;
-			}else{
-				return 0;
-			}
-		};
-	}else{
-		byFieldFunction = function(b,a){return byField(a,b);};
-	}
-	return ret.sort(byFieldFunction);
+	return this.sort(byField,true);//true - descending order
 };
--- src/sort.js
+++ src/sort.js
@@ -1,24 +1,25 @@
+const getFieldFunction = require('./utils').getFieldFunction;
+
 /**
  * Sort the data collection in ascending order
  * @alias sort
- * @param   {string|number|function} byField The field to use for sorting - can be a function in which the data object is its first argument
+ * @param   {string|number|function} byField The field to use for sorting - can be a function in which the data object is its first argument and the return value is the value to sort upon
+ * @param   {boolean}                desc true if order should be descending. Default ascending
  * @returns {Collection}             The sorted collection
  */
-module.exports = function(data, byField){
+module.exports = function(data, byField,desc=false){
 	//create ret array
 	let ret = Array.from(data);
-	let byFieldFunction = byField;
-	if(typeof byField !== 'function'){
-		console.assert(typeof byField === 'string','sort argument should be comparing function or name of field');
-		byFieldFunction = function(a,b){
-			if(a[byField] < b[byField]){
-				return -1;
-			}else if(a[byField] > b[byField]){
-				return 1;
-			}else{
-				return 0;
-			}
-		};
-	}
+	const fieldFunction = getFieldFunction(byField);
+	byFieldFunction = function(a,b){
+		let rev = desc? -1 : 1;
+		if(fieldFunction(a) < fieldFunction(b)){
+			return -1 * rev;
+		}else if(fieldFunction(a) > fieldFunction(b)){
+			return 1 * rev;
+		}else{
+			return 0;
+		}
+	};
 	return ret.sort(byFieldFunction);
 };
