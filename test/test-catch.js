e6d6f14f5210ea89e7a28bca803cbcfcebb4bedc
elshor <elshor@gmail.com>
2018-03-11 12:37:01 +0200

    changed implementation of catch function

--- test/test-catch.js
+++ test/test-catch.js
@@ -3,8 +3,10 @@ const ds = require('..');
 const Collection = ds.Collection;
 
 t.test('test catch',(t)=>{
-	t.plan(1);
+	t.plan(2);
+	Collection(Promise.reject({message:'exception'})).catch((e)=>t.ok(e,'Caught single exception'));
 	Collection(Promise.resolve([1,2,3]))
-	.forEach((item)=>{if(item===2)throw new Error("Can't be 2");})
-	.catch((e)=>console.log('got',e));
+	.forEach((item)=>{
+		if(item===2)return Promise.reject("Can't be 2");
+	}).catch((e)=>console.log('got',e)||t.ok(e,'Caught exception in group'));
 });
